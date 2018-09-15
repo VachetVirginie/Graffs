e8746d13dd4c8bec5cb87bbb970fc8d682f0c76b
Vachet Virginie <vachetvirginie@gmail.com>
2018-09-15 10:42:38 +0200

    maj style

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,60 @@
+<!-- Resize screen to see responsive header image, text and arrows! -->
+
+<!-- Check out finished website at: https://scm.ulster.ac.uk/~B00664007/myworkspace/year2/des311/iconswebsite/ -->
+<style>
+
+body {
+    margin: 0 auto; /* this just resets things so everything is at edge of browser */
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    background-color: white;
+    color: red; 
+}
+
+#wrapper {
+    margin: 0 auto;
+    max-width: 960px;
+}
+
+/* ------ Header Section ------ */
+
+.heroimg {
+    max-width: 100%;
+    width: 100%;
+}
+
+.links > a {
+    color: red;
+    padding: 0 25px;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+
+.intro {
+    position: absolute;
+    text-align: center;
+    font-size: 2.2vw; /* responsive text - means 2.2% of viewport width*/
+    margin-top: 20%; /* this controls the position of the text - responsive! */
+    left: 50%;  
+    transform: translate(-50%, -50%);
+    color:black;
+}
+
+    
+}
+
+</style>
+<header>
+    
+    
+    <a href="#landmarkinfo">
+    </a>
+           
+    <img class="heroimg" src="/img/header.jpg">
+    
+    
+</header>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('admin')->group(function () {
     Route::resource ('category', 'CategoryController', [
